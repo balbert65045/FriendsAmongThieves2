37f967848d130481a94b99d853506b0301ac8e47
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2017-12-27 06:32:43 -0500

    Reorganized

---
+++ Assets/Buildings/InsideWindowPosition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsideWindowPosition : MonoBehaviour {
+
+    Window window;
+    float SpeedtoVault;
+    public void SetSpeedToVault(float speed)
+    {
+        SpeedtoVault = speed;
+    }
+
+    private void Start()
+    {
+        window = GetComponentInParent<Window>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Player>())
+        {
+            // 1 is Inside;
+            other.gameObject.GetComponent<Player>().VaultAreaInside = true;
+            other.gameObject.GetComponent<Player>().InVaultArea(this.transform.position, transform.parent, SpeedtoVault);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<Player>())
+        {
+            other.gameObject.GetComponent<Player>().VaultAreaInside = false;
+        }
+    }
+}
---
+++ Assets/Buildings/Window.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    float UpwardsSpeedforVaultOutside = 0f;
+    [SerializeField]
+    float UpwardsSpeedforVaultinside= 0f;
+
+
+    OutideWindowPosition outsideWindowPosition;
+    InsideWindowPosition insideWindowPosition;
+
+
+    public void playerUsingWindow(int location, GameObject player)
+    {
+        //1 is inside 
+        //2 is outside
+        if (location == 1)
+        {
+
+            Debug.Log("Player at Window Inside");
+        }
+        else if (location == 2)
+        {
+          //  player.transform.LookAt(this.transform);
+            Debug.Log("Player at Window Outside");
+        }
+    }
+
+
+	void Start () {
+        outsideWindowPosition = GetComponentInChildren<OutideWindowPosition>();
+        //outsideWindowPosition.SetSpeedToVault(UpwardsSpeedforVaultOutside);
+
+        insideWindowPosition = GetComponentInChildren<InsideWindowPosition>();
+     //   insideWindowPosition.SetSpeedToVault(UpwardsSpeedforVaultinside);
+    }
+	
+
+
+
+	// Update is called once per frame
+	void Update () {
+        outsideWindowPosition.SetSpeedToVault(UpwardsSpeedforVaultOutside);
+        insideWindowPosition.SetSpeedToVault(UpwardsSpeedforVaultinside);
+    }
+}
