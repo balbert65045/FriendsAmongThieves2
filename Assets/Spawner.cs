5f3748dc7c552a58dbff4d6eae143c85e1d82886
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2018-01-08 08:13:40 -0500

    added a spawner on the server

---
+++ Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Spawner : NetworkBehaviour {
+
+    // Use this for initialization
+    public override void OnStartServer()
+    {
+        Debug.Log("ServerStart");
+        myNetworkManager MynetworkManager = FindObjectOfType<myNetworkManager>();
+        DoorLocations doorlocals = FindObjectOfType<DoorLocations>();
+        GameObject door = Instantiate(MynetworkManager.spawnPrefabs[1], doorlocals.doorPositions[0].position, doorlocals.doorPositions[0].rotation);
+        NetworkServer.Spawn(door);
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/myNetworkManager.cs
+++ Assets/myNetworkManager.cs
@@ -15,16 +15,14 @@ public class myNetworkManager : NetworkManager {
     {
         base.OnStartHost();
 
-        //door.transform.position = doorlocals.doorPositions[0].position;
-        //door.transform.rotation = doorlocals.doorPositions[0].rotation;
         //DoorLocations doorlocals = FindObjectOfType<DoorLocations>();
-
         //GameObject door = Instantiate(spawnPrefabs[1], doorlocals.doorPositions[0].position, doorlocals.doorPositions[0].rotation);
-        //Debug.Log(door.transform.position);
-        //Debug.Log(doorlocals.doorPositions[0].position);
+        //NetworkServer.Spawn(door);
 
         Debug.Log(Time.timeSinceLevelLoad + " Host requested");
     }
+
+   
 
     public override void OnStartClient(NetworkClient myClient)
     {
@@ -38,9 +36,6 @@ public class myNetworkManager : NetworkManager {
         Debug.Log(Time.timeSinceLevelLoad + " Client connected to IP:" + conn.address);
         Debug.Log(Network.player.ipAddress);
 
-        DoorLocations doorlocals = FindObjectOfType<DoorLocations>();
-        GameObject door = Instantiate(spawnPrefabs[1], doorlocals.doorPositions[0].position, doorlocals.doorPositions[0].rotation);
-        NetworkServer.Spawn(door);
 
     }
 }
